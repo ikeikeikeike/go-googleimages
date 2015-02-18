c2713b07286698dec6e9f248886893eafedb9374
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-02-18 19:13:35 +0900

    added unitest

--- googleimages_test.go
+++ googleimages_test.go
@@ -13,29 +13,29 @@ func TestSimple(t *testing.T) {
 	}
 
 	if len(data.ResponseData.Results) <= 0 {
-		t.Fatalf("Unexpected Googleimages.ResponseData.Results: %d length", len(data.ResponseData.Results))
+		t.Errorf("Unexpected Googleimages.ResponseData.Results: %d length", len(data.ResponseData.Results))
 	}
 
 	if data.ResponseStatus <= 0 || data.ResponseStatus == 500 {
-		t.Fatalf("Unexpected Googleimages.ResponseStatus: %s", data.ResponseStatus)
+		t.Errorf("Unexpected Googleimages.ResponseStatus: %s", data.ResponseStatus)
 	}
 
 	if data.ResponseData.Cursor.EstimatedResultCount == "" {
-		t.Fatalf(
+		t.Errorf(
 			"Unexpected Googleimages.Cursor.ResponseData.EstimatedResultCount: %s length",
 			data.ResponseData.Cursor.EstimatedResultCount,
 		)
 	}
 
 	if len(data.ResponseData.Cursor.Pages) <= 0 {
-		t.Fatalf(
+		t.Errorf(
 			"Unexpected Googleimages.Cursor.ResponseData.Cursor.Pages: %d length",
 			len(data.ResponseData.Cursor.Pages),
 		)
 	}
 
 	if len(data.ResponseData.Results) <= 0 {
-		t.Fatalf(
+		t.Errorf(
 			"Unexpected Googleimages.ResponseData.Results: %d length",
 			len(data.ResponseData.Results),
 		)
@@ -44,33 +44,33 @@ func TestSimple(t *testing.T) {
 	r := data.ResponseData.Results[0]
 
 	if r.Width == "" {
-		t.Fatalf("Unexpected Result.Width: %s", r.Width)
+		t.Errorf("Unexpected Result.Width: %s", r.Width)
 	}
 	if r.Height == "" {
-		t.Fatalf("Unexpected Result.Height: %s", r.Height)
+		t.Errorf("Unexpected Result.Height: %s", r.Height)
 	}
 	if r.TbWidth == "" {
-		t.Fatalf("Unexpected Result.TbWidth: %s", r.TbWidth)
+		t.Errorf("Unexpected Result.TbWidth: %s", r.TbWidth)
 	}
 	if r.TbHeight == "" {
-		t.Fatalf("Unexpected Result.TbHeight: %s", r.TbHeight)
+		t.Errorf("Unexpected Result.TbHeight: %s", r.TbHeight)
 	}
 	if r.UnescapedUrl == "" {
-		t.Fatalf("Unexpected Result.UnescapedUrl: %s", r.UnescapedUrl)
+		t.Errorf("Unexpected Result.UnescapedUrl: %s", r.UnescapedUrl)
 	}
 	if r.Url == "" {
-		t.Fatalf("Unexpected Result.Url: %s", r.Url)
+		t.Errorf("Unexpected Result.Url: %s", r.Url)
 	}
 	if r.TitleNoFormatting == "" {
-		t.Fatalf("Unexpected Result.TitleNoFormatting: %s", r.TitleNoFormatting)
+		t.Errorf("Unexpected Result.TitleNoFormatting: %s", r.TitleNoFormatting)
 	}
 	if r.OriginalContextUrl == "" {
-		t.Fatalf("Unexpected Result.OriginalContextUrl: %s", r.OriginalContextUrl)
+		t.Errorf("Unexpected Result.OriginalContextUrl: %s", r.OriginalContextUrl)
 	}
 	if r.ContentNoFormatting == "" {
-		t.Fatalf("Unexpected Result.ContentNoFormatting: %s", r.ContentNoFormatting)
+		t.Errorf("Unexpected Result.ContentNoFormatting: %s", r.ContentNoFormatting)
 	}
 	if r.TbUrl == "" {
-		t.Fatalf("Unexpected Result.TbUrl: %s", r.TbUrl)
+		t.Errorf("Unexpected Result.TbUrl: %s", r.TbUrl)
 	}
 }
